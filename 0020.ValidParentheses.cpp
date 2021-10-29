5160506c5eb0b34c30a3bae275b6cab5b8431b86
a3828162 <76466788+a3828162@users.noreply.github.com>
2021-10-29 20:56:29 +0800

    20211029

--- 0020.ValidParentheses.cpp
+++ 0020.ValidParentheses.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isValid(string s) {
+        
         stack<char> dic;
 
         for (char& a : s)
             if (a == '(' || a == '[' || a == '{') dic.push(a);
             else
             {
-
-                if (!dic.empty() && ((a == ')' && dic.top() == '(') || (a == ']' && dic.top() == '[') || (a == '}' && dic.top() == '{'))) { dic.pop(); continue; }
+                if (!dic.empty() && (dic.top()-a==2||dic.top()-a==1)) { dic.pop(); continue; }
                 return false;
             }
 
